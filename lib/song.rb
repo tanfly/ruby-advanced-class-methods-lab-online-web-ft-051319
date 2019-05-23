ac2c8ae75ee1b16e691562385344e86b72193027
Stella McEwen <mcewen.stella@gmail.com>
2019-05-23 00:44:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,11 +35,10 @@ class Song
     end
     
     def self.find_or_create_by_name(name)
-      @@all.each do |song|
-        if song == name 
-          self.find_by_name(name) 
-        else 
-          self.create_by_name(name)
+      if self.find_by_name(name) == true 
+        self.find_by_name(name)
+      else 
+        self.create_by_name(name)
         end
       end
     end
