6c4a5db10d48ca9400e0936dcaa13e291166142f
Stella McEwen <mcewen.stella@gmail.com>
2019-05-23 00:38:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,9 +31,7 @@ class Song
     end
     
     def self.find_by_name(name)
-      @@all.each do |songs|
-        if songs == name 
-          return songs 
+      @@all.find
         end
       end
     end
