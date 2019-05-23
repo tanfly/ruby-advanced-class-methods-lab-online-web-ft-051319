13e1d7720ac93ea4701c3825f27065ac1f027a02
Stella McEwen <mcewen.stella@gmail.com>
2019-05-23 00:53:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,8 +44,8 @@ class Song
       
       def self.new_from_filename(name)
         song = self.new
-        song.name = (name.split("-")[1].chomp[".mp3"])
-        song.artist_name = name.split("-")[0]
+        song.name = (name.split("-")[1].chomp(".mp3"))
+        song.artist_name = (name.split("-")[0])
     
      
 
